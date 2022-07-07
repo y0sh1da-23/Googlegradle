ea4b833bf4daa1d3c3aa8f23f0bfe8617b18ccc7
y0sh1da-23 <dbancsik@gmail.com>
2022-07-07 10:41:33 +0300

    initial commit

---
+++ src/test/java/devops/example/gradledemo/GradledemoApplicationTests.java
@@ -0,0 +1,13 @@
+package devops.example.gradledemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradledemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
